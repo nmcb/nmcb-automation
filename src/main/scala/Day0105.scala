e8eee982302a8cf76bf88226258f8fc455369ab5
nmcb <nmcb@xs4all.nl>
2024-01-05 09:23:25 +0100

    baselining

---
+++ src/main/scala/Day0105.scala
@@ -0,0 +1,36 @@
+import scala.io.*
+
+object Day0105 extends App:
+
+  val day: String =
+    this.getClass.getSimpleName.init match
+      case s"Day$name" => name
+
+  val tasks: List[Task] =
+    Source
+      .fromResource(s"nmcb$day.txt")
+      .getLines
+      .map(s => {
+        println(s"1) $s"); s
+      })
+      .filterNot(_.isBlank)
+      .map(s => {
+        println(s"2) $s"); s
+      })
+      .filterNot(_.trim.startsWith("--"))
+      .map(s => {
+        println(s"3) $s"); s
+      })
+      .map:
+        case s"$name=$done" => Task(name, done.toBoolean)
+      .toList
+
+  case class Task(name: String, done: Boolean = false)
+
+  val start1: Long =
+    System.currentTimeMillis
+
+  val tasksDone: Int =
+    tasks.count(_.done)
+
+  println(s"Answer day $day part 1: ${tasksDone} [${System.currentTimeMillis - start1}ms]")
